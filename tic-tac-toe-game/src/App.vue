1b3b0eefcec5534a24d4f9d48049fa01c4be863a
lycho33 <cho.lyds@gmail.com>
2022-07-08 14:33:17 -0400

    include HTML

--- tic-tac-toe-game/src/App.vue
+++ tic-tac-toe-game/src/App.vue
@@ -1,6 +1,21 @@
 <script setup>
+  import {ref, computed } from 'vue'
+
+  const player = ref('X');
+  const board = ref([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ])
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <main class="pt-8 text-center">
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+
+    <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+  </main>
 </template>
+
+<style>
+</style>
